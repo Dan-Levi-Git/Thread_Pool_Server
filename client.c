b85b8c55ee63fb689b26ab8c20a2c145f5d6dea0
Dan-Levi-Git <Dan.md1@gmail.com>
2021-03-03 12:49:50 +0200

    client side

--- client.c
+++ client.c
@@ -16,7 +16,20 @@
 #include <sys/ioctl.h>
 #include <ctype.h>
 
-#define SERVER_PORT 9696
+
+#define SOCKTERROR -1
+
+/* Check function for errors in socket. */
+int check(int exp , const char* msg)
+{
+    if(exp == SOCKTERROR)
+    {
+        perror(msg);
+        exit(1);
+    }
+    return exp;
+}
+
 
 int main(int argc , char *argv[])
 {
@@ -24,48 +37,48 @@ int main(int argc , char *argv[])
     struct sockaddr_in serveraddr;
     struct hostent *server;
     char buffer[256];
+
      if (argc < 3)
      {
-         printf("error plaes provied legal host\n");
+         printf("Please provied legal host \n");
+         exit(1);
      }
 
-
+    /* Geting thr port num from shell. */
     portnum = atoi(argv[2]);
-    
-    sockfd = socket(AF_INET, SOCK_STREAM , 0);
-    if( sockfd < 0)
-    {
-        printf("faield open socket \n");
-    }
 
+    check(sockfd = socket(AF_INET, SOCK_STREAM , 0),"Failed creaet socket");
+    
+    /* Gating the host name by shell. */
     server = gethostbyname(argv[1]);
+
 
     if(server == NULL)
     {
-        printf("ERROR: server name... \n");
-
+        printf("Server name not legal try again ... \n");
+        exit(1);
     }
-    
+
+    /* Initialize the address struct. */
     bzero((char*)&serveraddr , sizeof(serveraddr));
     serveraddr.sin_family = AF_INET;
     serveraddr.sin_port = htons(portnum);
     bcopy((char*)server->h_addr,(char*)&serveraddr.sin_addr.s_addr, server->h_length);
 
-    if(connect(sockfd, (struct sockaddr*)& serveraddr, sizeof(serveraddr)) < 0)
-    {
-        printf("error connecting to sockt \n");
-    }
+    check(connect(sockfd, (struct sockaddr *)&serveraddr, sizeof(serveraddr)), "Failed connecting to sockt \n");
 
-    bzero(buffer , 256);
+    /* Read from file creaeted it  on the client machine ,and print is content. */
+    bzero(buffer, 256);
     FILE *fd;
     read(sockfd,buffer,256);
     int stat;
     fd = fopen("test1_recive.txt", "a");
     fwrite(buffer, sizeof(char), strlen(buffer), fd);
-    printf("receive file.the content is: %s \n",buffer);
+    printf("Receive file successfuly. \nThe content is: %s \n", buffer);
     fclose(fd);
-    
-    while(1)
+    printf("Now you can chat with the server:\n");
+
+     while (1)
     {
         bzero(buffer ,256);
         fgets(buffer,sizeof(buffer),stdin);
@@ -76,7 +89,3 @@ int main(int argc , char *argv[])
             break;
         }
     }
-    
-    close(sockfd);
-    return 0;
-}
