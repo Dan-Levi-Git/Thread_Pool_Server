5b96cc7ec2cec1338e118f0b611e5c83aef027af
Dan-Levi-Git <Dan.md1@gmail.com>
2021-03-03 12:31:35 +0200

    queue header

--- queue.h
+++ queue.h
@@ -1,18 +1,21 @@
 #include <stdio.h>
 
+
+/* Node struct of the link list. */
 typedef struct node
 {
     int *client_socket;
     struct node* next;
 }node;
 
+/* Queue struct. */
 typedef struct queue_node
 {
     struct node *head;
     struct node *tail;
 } queue_node;
 
-
+/* Queue API. */
 queue_node* init_queue ();
 void enqueu(int*clint_sock, queue_node* queue_pool);
 int *Dequeue(queue_node *queue_pool);
